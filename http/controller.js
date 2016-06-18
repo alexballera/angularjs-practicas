d3b99907e2d74119e27fdf8263e2fb7f9d7522c8
Alex Ballera <alexballera@gmail.com>
2016-06-18 17:57:41 -0400

    Agregado metodo Post de HTTP con AngularJS

--- http/controller.js
+++ http/controller.js
@@ -1,12 +1,26 @@
 angular.module('myFirstApp', [])
 .controller('FirstController', function ($scope, $http) {
   $scope.posts = []
+  $scope.newPost = {}
   $http.get('http://jsonplaceholder.typicode.com/posts')
   .success(function (data) {
     console.log(data)
     $scope.posts = data
   })
   .error(function (err) {
-    /* Act on the event */
   })
+  $scope.addPost = function () {
+    $http.post('http://jsonplaceholder.typicode.com/posts', {
+      title: $scope.newPost.title,
+      body: $scope.newPost.body,
+      userId: 1
+    })
+    .success(function (data, status, headers, config) {
+      $scope.posts.push(data)
+      $scope.newPost = {}
+    })
+    .error(function (error, status, headers, config) {
+      console.log(error)
+    })
+  }
 })
