6c5894699f013c74470f9ab6701f39bb82de2f30
Alex Ballera <alexballera@gmail.com>
2016-06-18 17:30:40 -0400

    Agregado metodo get de HTTP

--- http/controller.js
+++ http/controller.js
@@ -1,4 +1,12 @@
 angular.module('myFirstApp', [])
-.controller('FirstController', ['$scope', '$http', function (mv, $http) {
-
-}])
+.controller('FirstController', function ($scope, $http) {
+  $scope.posts = []
+  $http.get('http://jsonplaceholder.typicode.com/posts')
+  .success(function (data) {
+    console.log(data)
+    $scope.posts = data
+  })
+  .error(function (err) {
+    /* Act on the event */
+  })
+})
