5deb0a5e4ddd9f08160ad3ecf796c40010871e2a
Alex Ballera <alexballera@gmail.com>
2016-06-19 15:28:14 -0400

    Creacion de directivas personalizadas

---
+++ directives/app.js
@@ -0,0 +1,21 @@
+angular.module('customDirective', [])
+.directive('backImg', function () {
+  return function ($scope, element, attrs) {
+    attrs.$observe('backImg', function (value) {
+      element.css({
+        'background': 'url(' + value + ')',
+        'background-position': 'center',
+        'background-size': 'cover'
+      })
+    })
+  }
+})
+.controller('AppCtrl', function ($scope, $http) {
+  $http.get('https://api.github.com/users/alexballera/repos')
+  .success(function (data) {
+    $scope.repos = data
+  })
+  .error(function (err) {
+    console.log(err)
+  })
+})
