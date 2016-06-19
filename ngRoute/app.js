63845c1c511bd740c2ecee1ab6144d4a0c5c2482
Alex Ballera <alexballera@gmail.com>
2016-06-19 17:45:42 -0400

    Agregado ngRoute

---
+++ ngRoute/app.js
@@ -0,0 +1,13 @@
+angular.module('customDirective', ['ngRoute'])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'ReposController',
+    templateUrl: 'templates/home.html'
+  })
+  .when('/repo/:name', {
+    controller: 'RepoController',
+    templateUrl: 'templates/repo.html'
+  })
+  .otherwise('/')
+})
--- ngRoute/directives.js
+++ ngRoute/directives.js
@@ -1,4 +1,4 @@
-angular.module('customDirective', [])
+angular.module('customDirective')
 .directive('myAutocomplete', function () {
   function link (scope, element, attrs) {
     $(element).autocomplete({
@@ -24,25 +24,6 @@ angular.module('customDirective', [])
         'background-position': 'center',
         'background-size': 'cover'
       })
-    })
-  }
-})
-.controller('AppCtrl', function ($scope, $http) {
-  $scope.repos = []
-  $http.get('https://api.github.com/users/alexballera/repos')
-  .success(function (data) {
-    $scope.repos = data
-    for (var i = data.length - 1; i >= 0; i--) {
-      var repo = data[i]
-      $scope.repos.push(repo.name)
-    }
-  })
-  .error(function (err) {
-    console.log(err)
-  })
-  $scope.optionSelected = function (data) {
-    $scope.$apply(function () {
-      $scope.main_repo = data
     })
   }
 })
